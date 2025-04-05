2178c9962122ac86f764aaa4051551c8ac442d26
FTNeves80 <ftneves80@gmail.com>
2025-04-04 22:05:53 -0300

    Registrando os dois modelos na mesma run do mlflow com subpatas diferentes

--- src/kobe/pipelines/treinamento/nodes.py
+++ src/kobe/pipelines/treinamento/nodes.py
@@ -2,20 +2,31 @@
 This is a boilerplate pipeline 'treinamento'
 generated using Kedro 0.19.12
 """
+import mlflow
+import mlflow.sklearn
 from pycaret.classification import ClassificationExperiment
+from mlflow.tracking import MlflowClient
 
-def treinar_dt(train_set, session_id):
-    exp = ClassificationExperiment()
-    exp.setup(data=train_set, target='shot_made_flag', session_id=session_id)
-    #include = ["lr", "nb", "rf", "et", "knn", "dt", "svm", "dummy"]
-    #model = exp.compare_models(include=include, sort="AUC")
-    model = exp.create_model('dt')
-    tuned_model = exp.tune_model(model, n_iter=100, optimize='AUC')
-    return tuned_model
+def treinar(train_set, session_id):
 
-def treinar_lr(train_set, session_id):
-    exp = ClassificationExperiment()
-    exp.setup(data=train_set, target='shot_made_flag', session_id=session_id)
-    model = exp.create_model('lr')
-    tuned_model = exp.tune_model(model, n_iter=100, optimize='AUC')
-    return tuned_model
+    # Renomeia o run principal criado automaticamente pelo Kedro
+    client = MlflowClient()
+    client.set_tag(mlflow.active_run().info.run_id, "mlflow.runName", "Modelos")
+
+    with mlflow.start_run(run_name="Decision Tree",nested=True):
+        # Setup do experimento
+        exp = ClassificationExperiment()
+        exp.setup(data=train_set, target='shot_made_flag', session_id=session_id)
+
+        # Treinamento do Decision Tree
+        model_dt = exp.create_model('dt')
+        tuned_model_dt = exp.tune_model(model_dt, n_iter=1, optimize='AUC')
+        mlflow.sklearn.log_model(tuned_model_dt, artifact_path="model_dt",registered_model_name="DecisionTree")  
+
+    with mlflow.start_run(run_name="Logistic Regression",nested=True):
+        # Treinamento do Logistic Regression
+        model_lr = exp.create_model('lr')
+        tuned_model_lr = exp.tune_model(model_lr, n_iter=1, optimize='AUC')
+        mlflow.sklearn.log_model(tuned_model_lr, artifact_path="model_lr",registered_model_name="LogisticRegression")  
+
+    return None
--- src/kobe/pipelines/treinamento/pipeline.py
+++ src/kobe/pipelines/treinamento/pipeline.py
@@ -8,16 +8,23 @@ from . import nodes
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
+        #node(
+        #    func=nodes.treinar_dt,
+        #    inputs=["train_set", "params:session_id"],
+        #    outputs="best_dt_model",
+        #    tags=["train", "decision_tree"]
+        #),
+        #node(
+        #    func=nodes.treinar_lr,
+        #    inputs=["train_set", "params:session_id"],
+        #    outputs="best_lr_model",
+        #    tags=["train", "logistic"]
+        #)
         node(
-            func=nodes.treinar_dt,
+            func=nodes.treinar,
             inputs=["train_set", "params:session_id"],
-            outputs="best_dt_model",
-            tags=["train", "decision_tree"]
-        ),
-        node(
-            func=nodes.treinar_lr,
-            inputs=["train_set", "params:session_id"],
-            outputs="best_lr_model",
-            tags=["train", "logistic"]
-        ),
+            outputs=None,
+            tags=["train"]
+        )
+
     ])
