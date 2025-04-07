eabb697c3d69fee7ddc14bac9d89aeb025b74c94
FTNeves80 <ftneves80@gmail.com>
2025-04-06 21:19:46 -0300

    Salvando logloss do pipeline inferencia no mlflow

--- src/kobe/pipelines/aplicacao/nodes.py
+++ src/kobe/pipelines/aplicacao/nodes.py
@@ -4,9 +4,13 @@ generated using Kedro 0.19.12
 """
 import pandas as pd
 import mlflow.pyfunc
+import mlflow.sklearn
+from sklearn.metrics import log_loss
 from datetime import date
 
 
+# Receber a base de teste para gerar o log_loss
+#
 def inferencia(raw_prod):
     print("######################## Estou rodando agora ##############")
     raw_data = raw_prod.dropna()
@@ -16,6 +20,16 @@ def inferencia(raw_prod):
     test_data = prod
     predictions = loaded_model.predict(test_data)
     predictions = pd.DataFrame(predictions, columns=["previsao"])
+    
+    print("######################## INICIO Log loss ##############")
+    loaded_model = mlflow.sklearn.load_model(model_uri)
+    pred_probs = loaded_model.predict_proba(test_data)[:, 1]
+    
+    y_true = raw_data['shot_made_flag']
+    logloss = log_loss(y_true, pred_probs)
+    print(f'pred_probs: {pred_probs}')
+    print(f'Log Loss: {logloss}')
+    print("######################## Termino Log loss ##############")
     
     print(predictions)
     
@@ -28,6 +42,7 @@ def inferencia(raw_prod):
         mlflow.log_param("colunas_usadas", list(prod.columns))
         mlflow.log_param("missing_dropped", raw_prod.isna().sum().sum())
         # Log de métricas simples
+        mlflow.log_metric("Log Loss", logloss)
         mlflow.log_metric("media_previsao", predictions["previsao"].mean())
         mlflow.log_metric("classe_1", (predictions["previsao"] == 1).sum())
         mlflow.log_metric("classe_0", (predictions["previsao"] == 0).sum())
